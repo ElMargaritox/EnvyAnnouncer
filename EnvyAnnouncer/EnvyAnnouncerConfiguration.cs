37c4873a79b20343ecdf02f0a02342b09972fe24
ElMargaritox <alexisguzman0303@gmail.com>
2021-11-18 00:43:49 -0300

    Agregar archivos de proyecto.

---
+++ EnvyAnnouncer/EnvyAnnouncerConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvyAnnouncer.Models;
+using Rocket.API;
+
+namespace EnvyAnnouncer
+{
+    public class EnvyAnnouncerConfiguration : IRocketPluginConfiguration
+    {
+        public int Time;
+        public bool RandomizeMessages;
+        public bool ShowInConsole;
+        public List<Message> Messages;
+        public void LoadDefaults()
+        {
+            Time = 300;
+            ShowInConsole = true;
+            RandomizeMessages = true;
+            Messages = new List<Message>
+            {
+                new Message{ Content = "(color=red)EnvyHosting - By Margarita#8172(/color)", Icon = "https://i.pinimg.com/736x/c5/38/01/c53801a34adcad784397180fd15d5638.jpg"}
+            };
+            
+
+        }
+    }
+}
---
+++ EnvyAnnouncer/EnvyAnnouncerPlugin.cs
@@ -0,0 +1,64 @@
+﻿using EnvyAnnouncer.Models;
+using Rocket.Core.Plugins;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using UnityEngine;
+
+namespace EnvyAnnouncer
+{
+    public class EnvyAnnouncerPlugin : RocketPlugin<EnvyAnnouncerConfiguration>
+    {
+        public static EnvyAnnouncerPlugin Instance;
+        private int contador;
+        private Timer timer;
+        protected override void Load()
+        {
+            Instance = this;
+            contador = 0;
+
+            timer = new Timer(1000 * Configuration.Instance.Time); timer.Elapsed += Timer_Elapsed; timer.AutoReset = true; timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+           
+
+
+            if (Configuration.Instance.RandomizeMessages)
+            {
+                Message message = Configuration.Instance.Messages.RandomOrDefault();
+                ChatManager.serverSendMessage(message.Content.Replace('(', '<').Replace(')', '>'), Color.white, null, null, EChatMode.GLOBAL, message.Icon, true);
+                if (Configuration.Instance.ShowInConsole) Console.WriteLine(message.Content);
+            }
+            else
+            {
+                try
+                {
+                    Message message = Configuration.Instance.Messages[contador];
+                    ChatManager.serverSendMessage(message.Content.Replace('(', '<').Replace(')', '>'), Color.white, null, null, EChatMode.GLOBAL, message.Icon, true);
+
+
+                    if (Configuration.Instance.ShowInConsole) Console.WriteLine(message.Content);
+                    contador++;
+                }
+                catch
+                {
+                    contador = 0;
+                }
+                
+            }
+        }
+
+
+        protected override void Unload()
+        {
+            timer.Stop(); timer.Elapsed -= Timer_Elapsed; timer.Dispose();
+            contador = 0;
+        }
+    }
+}
